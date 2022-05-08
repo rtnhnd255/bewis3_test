89ea33a3b7fa2c28153b7b25ad98efc028bf3da9
rtnhnd255 <rtn.hnd.256@gmail.com>
2022-05-08 20:00:27 +0500

    base

--- app/app.py
+++ app/app.py
@@ -1,20 +1,60 @@
-from crypt import methods
+from datetime import datetime
+from typing import List
 from urllib import response
+from xmlrpc.client import Boolean
+import flask
 import requests
-from flask import Flask, Response, request
+from flask import Flask, Response, jsonify, request
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from typing import List
+from app.model import Question, db
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app: flask.app.Flask = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://'
+db.init_app(app)
 
 @app.route("/", methods=["POST"])
 def handler():
-    question_number: int = request.args.get("questions_num", type=int) 
-    r = requests.get("https://jservice.io/api/random?count=1")
+    request_data = request.get_json()
+    question_amount: int = request_data["questions_num"] 
+    questions: List[Question] = request_questions(question_amount)
+    
+    lastq: Question = get_previous_question()
+    write_batch_questions(questions)
+    
     return Response (
-        r.text,
-        content_type=r.headers["content-type"]
+        jsonify(lastq)
     )
 
+def request_questions(amount: int) -> List[Question]:
+    request_datetime: datetime = datetime.now
+    result: List[Question] = []
+    
+    r = requests.get("https://jservice.io/api/random?count=%(amount)d"%{"amount": amount})
+    questions: dict = r.json
+    for i in questions:
+        q: Question = Question(i, request_datetime)
+        result.append(make_unique(q))
+    return result
+
+def make_unique(q: Question, dt: datetime) -> Question:
+    if Question.query.filter_by(id=q.id).first() != None :
+        r = requests.get("https://jservice.io/api/random?count=1")
+        qq = Question(r.json[0], dt)
+        return make_unique(qq, dt)
+    else:
+        return q
+
+def get_previous_question() -> Question:
+    q = Question.query.order_by("requested_at").first()
+    return q
+
+def write_batch_questions(questions: List[Question]):
+    for q in questions:
+        db.session.add(q)
+    db.session.commit()
+
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=False)
--- app/model.py
+++ app/model.py
@@ -1,14 +1,22 @@
-from datetime import date
+from datetime import datetime
+from logging.config import dictConfig
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String, Date
 
 Base = declarative_base()
-
+db = SQLAlchemy()
 class Question(Base):
     __tablename__ = "questions"
 
-    id: int = Column(Integer, primary_key=True)
-    question: str = Column(String)
-    answer: str = Column(String)
-    timestamp: date = Column(Date)
+    id: int = db.Column(db.Integer, primary_key=True)
+    question: str = db.Column(db.String())
+    answer: str = db.Column(db.String())
+    timestamp: datetime = db.Column(db.DateTime)
 
+    def __init__(self, i: dict):
+        self.id = i['id']
+        self.question = i['question']
+        self.answer = i['answer']
+        ss = i['created_at'].replace('T', " ").split(".")[0]
+        self.timestamp = datetime.strptime(ss, "%Y-%d-%m %H:%M:%S")
